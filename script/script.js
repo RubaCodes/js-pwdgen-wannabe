70596aad783dea82eb11345e908abda66753b395
rubacodes <rubacodes@gmail.com>
2022-04-27 14:36:07 +0200

    refactoring

--- script/script.js
+++ script/script.js
@@ -4,8 +4,8 @@ const userName = prompt('Inserisci il tuo nome:');
 const userLastName = prompt('Inserisci il tuo Cognome:');
 // poi chiedi il suo colore preferito
 const favoriteColor = prompt('Inserisci il tuo Colore preferito:');
-// Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+//store della password
 const password = `${userName}${userLastName}${favoriteColor}21`;
-
+//print della password sul documento
 document.getElementById('password').innerHTML = password;
