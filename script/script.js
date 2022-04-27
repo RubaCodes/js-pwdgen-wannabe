1219c4da9a0946a1a36215a20f0760d2d285802a
rubacodes <rubacodes@gmail.com>
2022-04-27 12:54:49 +0200

    Added promps, link css

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// Chiedi all’utente il suo nome,
+const userName = prompt('Inserisci il tuo nome:');
+// poi chiedi il suo cognome
+const userLastName = prompt('Inserisci il tuo Cognome:');
+// poi chiedi il suo colore preferito
+const favoriteColor = prompt('Inserisci il tuo Colore preferito:');
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
