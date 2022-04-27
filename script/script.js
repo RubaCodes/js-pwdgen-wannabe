39db8a356237d3542c9fa1f47d421eab8be04982
rubacodes <rubacodes@gmail.com>
2022-04-27 14:20:39 +0200

    addesd style and passowrd generator

--- script/script.js
+++ script/script.js
@@ -5,3 +5,7 @@ const userLastName = prompt('Inserisci il tuo Cognome:');
 // poi chiedi il suo colore preferito
 const favoriteColor = prompt('Inserisci il tuo Colore preferito:');
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+const password = `${userName}${userLastName}${favoriteColor}21`;
+
+document.getElementById('password').innerHTML = password;
