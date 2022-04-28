9769d4a3a33cc540430b75820d3e70f1451e3a18
rubacodes <rubacodes@gmail.com>
2022-04-28 07:13:13 +0200

    reimplementazione Password random con Fisher-Yates

--- script/script.js
+++ script/script.js
@@ -11,6 +11,28 @@ const password = `${userName}${userLastName}${favoriteColor}21`;
 document.getElementById('password').innerHTML = password;
 
 //una password piu random partendo sempre dall'input,grazie stackoverflow ,anche se il randomizer non e' cosi' random
-let shuffledPassword = password.split('').sort(function(){return 0.5-Math.random()}).join('');
+//let shuffledPassword = password.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+//document.getElementById('shuffledPassword').innerHTML = shuffledPassword;
+
+
+//algoritmo tootalemente random :The Fisher-Yates algorith 
+const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
+const passwordToArray = password.split('');
+
+const shuffledPassword = shuffleArray(passwordToArray).join('');
 
 document.getElementById('shuffledPassword').innerHTML = shuffledPassword;
+
+
+console.log(password);
+console.log(shuffledPassword);
